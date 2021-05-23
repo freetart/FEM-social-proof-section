d774a2fe35e12f1f2521532a0e6adf08330f750b
Chris M <habibdev96@gmail.com>
2021-05-22 23:34:43 -0600

    created preloader

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const preloader = () => {
+  const preloaderEl = document.querySelector(".preloader");
+
+  window.addEventListener("load", function () {
+    preloaderEl.classList.add("hide-preloader");
+  });
+};
+preloader();
